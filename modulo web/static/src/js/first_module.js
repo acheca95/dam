06b9e5bb7c38943559c01f06d00b7049d19b2368
Antonio Checa <antonio_ch_95@hotmail.com>
2020-02-18 17:28:29 +0100

    fix error destroy
    fix error destroy

--- modulo web/static/src/js/first_module.js
+++ modulo web/static/src/js/first_module.js
@@ -1,5 +1,5 @@
 // static/src/js/first_module.js
-openerp.web_example = function (instance) {
+odoo.web_example = function (instance) {
     instance.web.client_actions.add('example.action', 'instance.web_example.Action');
     instance.web_example.Action = instance.web.Widget.extend({
         template: 'web_example.action',
@@ -11,65 +11,36 @@ openerp.web_example = function (instance) {
             this._super.apply(this, arguments);
             this._start = null;
             this._watch = null;
-            this.model = new instance.web.Model('web_example.stopwatch');
         },
-        start: function () {
-            var display = this.display_record.bind(this);
-            return this.model.query()
-                .filter([['user_id', '=', instance.session.uid]])
-                .all().done(function (records) {
-                    _(records).each(display);
-                });
-        },
-        current: function () {
-            // Subtracting javascript dates returns the difference in milliseconds
-            return new Date() - this._start;
-        },
-        display_record: function (record) {
-            $('<li>')
-                .text(this.format_time(record.time))
-                .appendTo(this.$('.oe_web_example_saved'));
-        },
-        format_time: function (time) {
+        update_counter: function () {
             var h, m, s;
-            s = time / 1000;
+            // Subtracting javascript dates returns the difference in milliseconds
+            var diff = new Date() - this._start;
+            s = diff / 1000;
             m = Math.floor(s / 60);
-            s -= 60 * m;
+            s -= 60*m;
             h = Math.floor(m / 60);
-            return _.str.sprintf("%02d:%02d:%02d", h, m, s);
-        },
-        update_counter: function (time) {
-            this.$('.oe_web_example_timer').text(this.format_time(time));
+            m -= 60*h;
+            this.$('.oe_web_example_timer').text(
+                _.str.sprintf("%02d:%02d:%02d", h, m, s));
         },
         watch_start: function () {
             this.$el.addClass('oe_web_example_started')
-                .removeClass('oe_web_example_stopped');
+                    .removeClass('oe_web_example_stopped');
             this._start = new Date();
             // Update the UI to the current time
-            this.update_counter(this.current());
+            this.update_counter();
             // Update the counter at 30 FPS (33ms/frame)
-            this._watch = setInterval(function () {
-                this.update_counter(this.current());
-            }.bind(this),
+            this._watch = setInterval(
+                this.proxy('update_counter'),
                 33);
         },
         watch_stop: function () {
-            var self = this;
             clearInterval(this._watch);
-            var time = this.current();
-            this.update_counter(time);
+            this.update_counter();
             this._start = this._watch = null;
             this.$el.removeClass('oe_web_example_started')
-                .addClass('oe_web_example_stopped');
-                var record = {
-         
-                user_id: instance.session.uid,
-                time: time,
-            };
-            return this.model.call('create', [record]).done(function () {
-            this.model.call('create', [record]).done(function () {
-                self.display_record(record);
-            });
+                    .addClass('oe_web_example_stopped');
         },
         destroy: function () {
             if (this._watch) {
